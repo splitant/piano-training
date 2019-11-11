6080750a82ac7f9b24c4e0b78727bb0516360874
Splitant <axel.depret.pro@gmail.com>
2019-11-11 23:00:43 +0100

    Init project directory

---
+++ src/settings/settings_chores.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+class SettingsChores:
+   ordered = True
+   
+   "timer in ms"
+   timer = 500
+
+   def __init__(self, name, salary):
+      self.name = name
+      self.salary = salary
+      Employee.empCount += 1
+   
+   def displayCount(self):
+     print "Total Employee %d" % Employee.empCount
+
+   def displayEmployee(self):
+      print "Name : ", self.name,  ", Salary: ", self.salary
