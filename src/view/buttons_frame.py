7e30c10198693595e89c04c97b77f51bce54b8a5
Axel DEPRET <axel.depret-prestataire@laposte.fr>
2019-12-19 18:23:18 +0100

    Center button on main frame

--- src/view/buttons_frame.py
+++ src/view/buttons_frame.py
@@ -12,15 +12,18 @@ class ButtonsFrame(tk.Frame):
 
     def create_widgets(self):
         self.button_play = tk.Button(self, text="Play",
-                                     fg="grey", command=self.quit)
+                                     fg="black", command=self.button_play_command)
         self.button_settings = tk.Button(self, text="Settings",
-                                         fg="grey", command=self.quit)
+                                         fg="black", command=self.button_settings_command)
         
-        self.button_play.grid(row=0)
-        self.button_settings.grid(row=1)
+        self.button_play.grid(row=0, column=0, sticky='nesw', pady=10)
+        self.button_settings.grid(row=1, column=0, sticky='nesw', pady=10)
         self.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
-
-        self.pack()
+    
+    def button_play_command(self):
+      
+    
+    def button_settings_command(self):
     
 
 
