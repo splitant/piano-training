b270add03f1c6c83822000f8aaa0fdb6c071a65b
Axel DEPRET <axel.depret-prestataire@laposte.fr>
2020-03-10 11:49:29 +0100

    Full screen mode

--- src/main.py
+++ src/main.py
@@ -5,8 +5,24 @@ except:
 
 import view as v
 
+class PianoTrainingMain:
+    def __init__(self):
+        self.window = v.ManagerFrames()
+        self.window.attributes('-fullscreen', True)
+        self.fullScreenState = False
+        self.window.bind("<F11>", self.toggleFullScreen)
+        self.window.bind("<Escape>", self.quitFullScreen)
+        self.window.geometry('500x500')
+        self.window.mainloop()
+
+    def toggleFullScreen(self, event):
+        self.fullScreenState = not self.fullScreenState
+        self.window.attributes("-fullscreen", self.fullScreenState)
+
+    def quitFullScreen(self, event):
+        self.fullScreenState = False
+        self.window.attributes("-fullscreen", self.fullScreenState)
+
 if __name__ == "__main__":
-    app = v.ManagerFrames()
-    app.title("Piano Training")
-    app.geometry("500x500")
-    app.mainloop()
+    app = PianoTrainingMain()
+
